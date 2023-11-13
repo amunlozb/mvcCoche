eff29eda19039fdbb18e67500806a06538d6d85a
amunlozb <amunlozb@g.educaand.es>
2023-11-13 07:29:39 +0100

    Initial commit

---
+++ src/main/java/com/crudsimulacro/CrudSimulacroApplication.java
@@ -0,0 +1,13 @@
+package com.crudsimulacro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudSimulacroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudSimulacroApplication.class, args);
+	}
+
+}
